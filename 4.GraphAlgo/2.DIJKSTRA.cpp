8f96832ccdc461300e143e614f9c938e084cfb2b
Abhishek Anand <87860680+abhianand987@users.noreply.github.com>
2022-03-25 17:23:46 +0530

    Create 2.DIJKSTRA.cpp

--- 4.GraphAlgo/2.DIJKSTRA.cpp
+++ 4.GraphAlgo/2.DIJKSTRA.cpp
@@ -1,34 +1,69 @@
 //Dijkstra Self Brute Force and Running On Sublime
 //By both Priority Queue and Set
 //On Leetcode set version is giving RUNTIME ERROOR.
+//Idea -1 Slight Optimization
+	// DIJKSTRA-1
+	void Dijkstra(long src, vector<pair<long, long>> adj[], vector<long>&dist)
+	{
+		priority_queue<pair<long, long>, vector<pair<long, long>>, greater<pair<long, long>>> pq;
+		pq.push({0, src});
+		minDistanceFromSrcTo[src] = 0;
+		long u, v, wt, currdist;
+		while (!pq.empty()) {
+			u = pq.top().second;
+			currdist = pq.top().first;
+			pq.pop();
+			// below line in Dijkstra becomes game changer
+			// because if for any pair in priority_queue pq
+			// minDistanceFromSrcTo[u] < dist
+			// then shortest path passing from this u is already processed
+			// so no need to go below for() loop for time consumption
+			if (dist[u] < dist) continue; //Good Optimization
+         
+         
+			for (auto it : adj[u]) {
+				v = it.first;
+				wt = it.second;
+				if (dist[u] + wt < dist[v]) {
+					dist[v] = wt + dist[u];
+					pq.push({dist[v], v});
+				}
+			}
+		}
+	}
+// DIJKSTRA-2 More Optimizations
+void Dijkstra(long src, vector<pair<long, long>> adj[], vector<long>&dist)
+	{
+		priority_queue<pair<long, long>, vector<pair<long, long>>, greater<pair<long, long>>> pq;
+		pq.push({0, src});
+		minDistanceFromSrcTo[src] = 0;
+		long u, v, wt, currdist;
+		while (!pq.empty()) {
+			u = pq.top().second;
+			currdist = pq.top().first;
+			pq.pop();
+         
+         vis[u] = true; //More Optimization
+         
+			
+			if (dist[u] < dist) continue;
+			for (auto it : adj[u]) {
+				v = it.first;
+            
+            if(vis[v]) continue;
+            
+				wt = it.second;
+				if (dist[u] + wt < dist[v]) {
+					dist[v] = wt + dist[u];
+					pq.push({dist[v], v});
+				}
+			}
+		}
+	}
 
-using namespace std;
-#include<bits/stdc++.h>
-#define ll long long
-#define pii pair<int,int>
-class Graph{
-   //vector<pii>*l;
-   vector<vector<pii>>l;
-   int V;
-public:
-   Graph(int V){
-      this->V = V;
-      //l = new vector<pii>[V];
-      l = vector<vector<pii>>(V);
-   }
-   void addEdge(int u,int v,int w){
-      l[u].push_back({v,w});
-      l[v].push_back({u,w});
-   }
-   void printGraph(){
-      for(int i = 0 ; i < V;i++){
-         cout<<"Vertex is "<<i<<"  --->";
-         for(auto nbrPair : l[i]){
-            cout<<"("<<nbrPair.first<<","<<nbrPair.second<<")";
-         }
-         cout<<endl;
-      }
-   }
+
+
+
    void dijkstra(int src){
 
       vector<int>dist(V,INT_MAX);
@@ -46,25 +81,25 @@ public:
          int node = temp->second;*/
          auto temp = pq.top(); pq.pop();
          int distTill = temp.first;
-         int node = temp.second;
+         int u = temp.second;
          vis[node] = true;
 
 
          for(auto nbrPair : l[node]){
 
             int edgeWeight = nbrPair.second;
-            int node = nbrPair.first;
+            int v = nbrPair.first;
             
-            if(vis[node] == true) 
+            if(vis[v] == true) 
                continue;
 
 
-            if(distTill + edgeWeight < dist[node]){
+            if(dist[u] + edgeWeight < dist[v]){
 
 
-               dist[node] = distTill + edgeWeight;
+               dist[v] = dist[u] + edgeWeight;
                //s.insert({dist[node],node});
-               pq.push({dist[node],node});
+               pq.push({dist[v],v});
             }
          }
       }
